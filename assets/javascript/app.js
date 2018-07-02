281ddaf565da19efa306e61fd7adad74c4187275
ACE Merc <acendelman@gmail.com>
2018-07-02 09:20:45 -0400

    everything together but timer doesn't display and styling not final

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,75 +1,86 @@
-//create a timer and have it run down
+//creates start button (and then hides it)<----TODO
+$("#start").click(askQuestion);
+
 //insert inbetween with correct or incorrect and timed... maybe giphy link
 
 var correct = 0;
 var incorrect = 0;
 var questionsLeft = 8;
+var setTimeOut;
+
 
 //Sets up all the questions as an object then within them every question as an object array
 
 var allQuestions = [
     {
-        question: "The sky is what color?",
-        correct: "Blue",
-        incorrect: "Red",
-        incorrecter: "Green",
-        incorrectest: "Orange",
+        question: "A helicopter is used to dry out this fruit after a rainy day, to prevent it from splitting open...",
+        correct: "Cherry",
+        incorrect: "Banana",
+        incorrecter: "Tomato",
+        incorrectest: "Plum",
     }, {
-        question: "Which of the following is a fruit?",
+        question: "There is a national _____ reserve, in case supply exceeds demand for this dried berry.",
+        correct: "Raisin",
+        incorrect: "Goji Berry",
+        incorrecter: "Blueberry",
+        incorrectest: "Cranberry"
+    }, {
+        question: "The word for the color orange wasn’t created until 1542, and was a combination of these two names for the fruit.",
+        correct: "Naranj and narang",
+        incorrect: "Orani and Nange",
+        incorrecter: "Onge and Norgj",
+        incorrectest: "Nrang and Orge"
+
+    }, {
+        question: "Farmer’s pick this fruit before it is ready, then use technology to trick it into ripening before hitting the shelves.",
         correct: "Banana",
-        incorrect: "Gas",
-        incorrecter: "Computer",
-        incorrectest: "Hair"
-    }, {
-        question: "How many fingers does a normal human have?",
-        correct: "10",
-        incorrect: "5",
-        incorrecter: "0",
-        incorrectest: "Most of them"
+        incorrect: "Apples",
+        incorrecter: "Avocado",
+        incorrectest: "Mango"
 
     }, {
-        question: "All roads lead to?",
-        correct: "Rome",
-        incorrect: "Roam",
-        incorrecter: "Sadness",
-        incorrectest: "Yo Mama"
+        question: "In 45 A.D., this fruit was originally called “ida” after the mountain it was grown on.",
+        correct: "Raspberry",
+        incorrect: "Apricot",
+        incorrecter: "Pomegranate",
+        incorrectest: "Dragonfruit"
 
     }, {
-        question: "Rinse, Lather, ____?",
-        correct: "Repeat",
-        incorrect: "Flush",
-        incorrecter: "Hold your horses",
-        incorrectest: "Who cares?"
+        question: "There are over 700 varieties of peaches in the world, but peaches are the most respected and admired in this country.",
+        correct: "China",
+        incorrect: "Canada",
+        incorrecter: "USA",
+        incorrectest: "Belgium"
 
     }, {
-        question: "What color are lemons?",
-        correct: "Yellow(ish)",
-        incorrect: "Green(ish)",
-        incorrecter: "Purple(ish)",
-        incorrectest: "Brown"
+        question: "What is the most-consumed fruit in the world?",
+        correct: "Mangos",
+        incorrect: "Bananas",
+        incorrecter: "Nectarines",
+        incorrectest: "Tomatoes"
 
     }, {
-        question: "In what year did America first reach the moon?",
-        correct: "1969",
-        incorrect: "1971",
-        incorrecter: "500",
-        incorrectest: "2090"
-
-    }, {
-        question: "2+2 equals?",
-        correct: "4",
-        incorrect: "22",
-        incorrecter: "5",
-        incorrectest: "0"
+        question: "What are the small edible pieces of the pomegranate called?",
+        correct: "Arils",
+        incorrect: "Pips",
+        incorrecter: "Cheeks",
+        incorrectest: "Donsils"
     }];
 
 console.log(allQuestions);
-askQuestion();
 
 function askQuestion() {
-
+    $("#start").empty();
     $("#answerButtons").empty();
     $("#question").empty();
+    $("#scoreSheet").empty();
+    $("#images").empty();
+    clearInterval(setTimeOut);
+
+    //creates the timer count for question
+
+    setTimeOut = setInterval(timeOut, 7000);
+    $("#timer").append(setTimeOut);
 
     //Chooses a random question from the array and assigns it to questionChoice to later be accessed in for loop to create buttons
 
@@ -150,21 +161,17 @@ function askQuestion() {
 
             if (result === "true") {
                 alert("Correct!");
-                correct++;
                 console.log(correct);
-                questionsLeft--;
                 delete questionChoice;
-                triviaCont();
+                showCorrectResult();
 
             }
 
             else if (result === "false") {
                 alert("Wrong!");
-                incorrect++;
                 console.log(incorrect);
-                questionsLeft--;
                 delete questionChoice;
-                triviaCont();
+                showIncorrectResult();
             }
         });
 
@@ -188,8 +195,99 @@ function triviaCont() {
         $("#answerButtons").empty();
         $("#question").empty();
 
-        document.write("Game Over! Correct: " + correct + " and Incorrect: " +incorrect);
+        document.write("Game Over! Correct: " + correct + " and Incorrect: " + incorrect);
     }
 
     //should be slideshow loop then next question or if no questions left game over score...
 }
+
+function timeOut() {
+    clearInterval(setTimeOut);
+    delete questionChoice;
+    showIncorrectResult();
+}
+
+function showCorrectResult() {
+    $("#answerButtons").empty();
+    $("#question").empty();
+    correct++;
+    questionsLeft--;
+    getGiphy();
+    $("#scoreSheet").append("Total Correct:" +correct+ "and Total Incorrect:" +incorrect);
+    clearInterval(setTimeOut);
+
+    //checks if game over or repeat
+
+    if (questionsLeft > 0) {
+        //  (append scoresheet to show applause and scoresheet)
+        setTimeOut = setInterval(askQuestion, 4000);
+    }
+
+    else if (questionsLeft = 0) {
+        gameOver();
+    }
+
+}
+
+function showIncorrectResult() {
+    $("#answerButtons").empty();
+    $("#question").empty();
+    questionsLeft--;
+    incorrect++;
+    getGiphy();
+    $("#scoreSheet").append("Total Correct: " + correct + "   and Total Incorrect: " + incorrect);
+    clearInterval(setTimeOut);
+
+    if (questionsLeft > 0) {
+        //  (append scoresheet to show boo and scoresheet)
+        setTimeOut = setInterval(askQuestion, 4000);
+    }
+
+    else if (questionsLeft = 0) {
+        gameOver();
+    }
+
+}
+
+//creates giphy for correct answer
+
+function getGiphy() {
+
+var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=fruits";
+
+// Perfoming an AJAX GET request to our queryURL
+$.ajax({
+  url: queryURL,
+  method: "GET"
+})
+
+// After the data from the AJAX request comes back
+  .then(function(response) {
+
+  // Saving the image_original_url property
+    var imageUrl = response.data.image_original_url;
+
+    // Creating and storing an image tag
+    var fruitImage = $("<img>");
+
+    // Setting the catImage src attribute to imageUrl
+    fruitImage.attr("src", imageUrl);
+    fruitImage.attr("alt", "fruit image");
+
+    // Prepending the catImage to the images div
+    $("#images").prepend(fruitImage);
+  });
+
+}
+
+function gameOver() {
+    //  create div for the final output
+    //  display ("correct: + correct")
+    //  display ("incorrect: + incorrect")
+
+}
+
+// creates timer and assigns variable/upon timeout calls time out and time length
+
+// setTimeOut = setInterval(timeOut, 10000);
+//     $("#timer").append(setTimeOut);
